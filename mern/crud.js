f3b8fc8839b6f789d0dcb516deff38b3917416dd
Rhishavhere <rhishh.me@gmail.com>
2026-07-16 00:57:14 +0530

    update

--- mern/crud.js
+++ mern/crud.js
@@ -90,3 +90,68 @@ createFile('Hi', (err, message) =>{
     })
   })
 })
+
+// OR
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const filePath = path.join(__dirname, 'myFile.txt'); //
+
+// const content = 'Hello, world!';
+// const newContent = 'This is the updated content.';
+
+// fs.writeFileSync(filePath, content);
+// console.log('File created successfully');
+
+// const data = fs.readFileSync(filePath, 'utf-8');
+// console.log(`File contents: ${data}`);
+
+// fs.writeFileSync(filePath, newContent);
+// console.log('File updated successfully');
+
+// fs.unlinkSync(filePath);
+// console.log('File deleted successfully');
+
+
+
+
+
+
+// const fs = require('fs/promises')
+// const path = require('path')
+
+// const folder = path.join(__dirname, 'demo')
+// const filePath = path.join(folder, 'example.txt')
+
+// async function run() {
+//   try {
+//     // Ensure folder exists
+//     await fs.mkdir(folder, { recursive: true })
+
+//     // Create
+//     await fs.writeFile(filePath, 'Hi')
+//     console.log('Written')
+
+//     // Read
+//     let data = await fs.readFile(filePath, 'utf8')
+//     console.log('Content:', data)
+
+//     // Update
+//     await fs.writeFile(filePath, 'Welcome')
+//     console.log('Updated')
+
+//     // Read again
+//     data = await fs.readFile(filePath, 'utf8')
+//     console.log('Content:', data)
+
+//     // Delete
+//     await fs.unlink(filePath)
+//     console.log('Deleted')
+
+//   } catch (err) {
+//     console.error('Error:', err)
+//   }
+// }
+
+// run()
